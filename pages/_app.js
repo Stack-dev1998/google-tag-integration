750b60d8b967086717db91bd38ca37ce7595ba8d
Hassan khan <hassanforfiver@gmail.com>
2023-08-03 14:55:39 +0500

    'sss'

--- pages/_app.js
+++ pages/_app.js
@@ -1,30 +1,71 @@
-import GoogleAnalytics from "@/components/GoogleAnalytics";
-import "../components/styles/tailwind.css";
-import { useRouter } from "next/router";
-import { useEffect } from "react";
-function MyApp({ Component, pageProps }) {
-  const router = useRouter();
-  const GA_TRACKING_ID = "G-6Z6TBTKXMQ"; // replace with your Google Analytics tracking ID
+// import GoogleAnalytics from "@/components/GoogleAnalytics";
+// import "../components/styles/tailwind.css";
+// import { useRouter } from "next/router";
+// import { useEffect } from "react";
+// function MyApp({ Component, pageProps }) {
+//   const router = useRouter();
+//   const GA_TRACKING_ID = "G-6Z6TBTKXMQ"; // replace with your Google Analytics tracking ID
 
+//   useEffect(() => {
+//     const handleRouteChange = (url) => {
+//       window.gtag("config", GA_TRACKING_ID, {
+//         page_path: url,
+//       });
+//     };
+
+//     router.events.on("routeChangeComplete", handleRouteChange);
+
+//     return () => {
+//       router.events.off("routeChangeComplete", handleRouteChange);
+//     };
+//   }, [router.events]);
+//   return (
+//     <>
+//       <GoogleAnalytics GA_TRACKING_ID={GA_TRACKING_ID} />
+//       <Component {...pageProps} />
+//     </>
+//   );
+// }
+
+// export default MyApp;
+
+import Head from "next/head";
+import { useRouter } from "next/router";
+import Script from "next/script";
+import { useEffect } from "react";
+import * as gtag from "../lib/gtag";
+import "../components/styles/tailwind.css";
+const App = ({ Component, pageProps }) => {
+  const router = useRouter();
   useEffect(() => {
     const handleRouteChange = (url) => {
-      window.gtag("config", GA_TRACKING_ID, {
-        page_path: url,
-      });
+      gtag.pageview(url);
     };
-
     router.events.on("routeChangeComplete", handleRouteChange);
-
     return () => {
       router.events.off("routeChangeComplete", handleRouteChange);
     };
   }, [router.events]);
+
   return (
     <>
-      <GoogleAnalytics GA_TRACKING_ID={GA_TRACKING_ID} />
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+        window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', '${gtag.GA_TRACKING_ID}');
+        `}
+      </Script>
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
       <Component {...pageProps} />
     </>
   );
-}
+};
 
-export default MyApp;
+export default App;
