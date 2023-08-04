b37568bde5b1e0c197c84451b13c8c71e47db351
Hassan khan <hassanforfiver@gmail.com>
2023-08-04 14:52:43 +0500

    facebook-pixel

--- pages/_app.js
+++ pages/_app.js
@@ -34,12 +34,16 @@ import { useRouter } from "next/router";
 import Script from "next/script";
 import { useEffect } from "react";
 import * as gtag from "../lib/gtag";
+import * as fbq from "../lib/fpixel";
 import "../components/styles/tailwind.css";
 const App = ({ Component, pageProps }) => {
   const router = useRouter();
   useEffect(() => {
+    fbq.pageview();
+
     const handleRouteChange = (url) => {
       gtag.pageview(url);
+      fbq.pageview();
     };
     router.events.on("routeChangeComplete", handleRouteChange);
     return () => {
@@ -49,6 +53,25 @@ const App = ({ Component, pageProps }) => {
 
   return (
     <>
+      {/* Global Site Code Pixel - Facebook Pixel */}
+      <Script
+        id="fb-pixel"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', ${fbq.FB_PIXEL_ID});
+          `,
+        }}
+      />
+
       {/* Global Site Tag (gtag.js) - Google Analytics */}
       <Script id="google-analytics" strategy="afterInteractive">
         {`
